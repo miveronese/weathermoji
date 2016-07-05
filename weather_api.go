e28d69c02f38660591d6280ce3d46aa2872b7413
herval <hervalfreire@gmail.com>
2016-07-04 20:38:48 -0700

    more weather data

--- weather_api.go
+++ weather_api.go
@@ -4,40 +4,47 @@ import (
   owm "github.com/briandowns/openweathermap"
   "log"
   "os"
+  "fmt"
 )
 
 type Forecast struct {
-  Humidity int
-  High     float64
-  Low      float64
+  Humidity  int
+  High      float64
+  Low       float64
   WindSpeed float64
+  Clouds    int
+  Weather   string
+  WeatherLevel string // TODO map from openweathermap.org/weather-conditions to some constants?
 }
 
 type WeatherApi struct {
   Location string
 }
 
-func NewWeatherApi(apiKey string) *WeatherApi {
-  os.Setenv("OWM_API_KEY", apiKey)
+func NewWeatherApi(apiKey string) WeatherApi {
+  os.Setenv("OWM_API_KEY", apiKey) // owm requires this env var to function
 
-  return &WeatherApi{
+  return WeatherApi{
     Location: "San Francisco, CA",
   }
 }
 
-func (w *WeatherApi) Now() Forecast {
-  weather, err := owm.NewCurrent("F", "EN") // fahrenheit (imperial) with Russian output
+func (self WeatherApi) Now() Forecast {
+  weather, err := owm.NewCurrent("F", "EN")
   if err != nil {
     log.Fatalln(err)
   }
 
-  weather.CurrentByName(w.Location)
+  weather.CurrentByName(self.Location)
 
   return Forecast{
     Humidity: weather.Main.Humidity,
     High: weather.Main.TempMax,
     Low: weather.Main.TempMin,
     WindSpeed: weather.Wind.Speed,
+    Clouds: weather.Clouds.All,
+    Weather: weather.Weather[0].Main,
+    WeatherLevel: weather.Weather[0].Description,
   }
 }
 
